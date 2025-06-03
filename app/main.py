f6d98006b958d98469b09b0d1fe87a7f1f18765a
bishal_giri <giribishal09@gmail.com>
2025-06-04 00:27:09 +0545

    feat: develop basic genz agent

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 from openai import OpenAI
 import os
 from dotenv import load_dotenv
+import copy
 
 # Load environment variables from .env
 load_dotenv()
@@ -15,15 +16,38 @@ app = FastAPI()
 class Message(BaseModel):
     message: str
 
+
+class ChatSession:
+    def __init__(self):
+        self.messages = []
+    
+    def add_message(self, message):
+        self.messages.append(message)
+
+    def get_messages(self):
+        return self.messages
+
+
+current_session = ChatSession()
+
+
 @app.post("/chat")
 async def chat(msg: Message):
+    
+    current_session.add_message({
+            'role': 'user',   
+            'content': msg.message
+    })
     response = client.responses.create(
         model="gpt-3.5-turbo", 
         instructions="You are a genz. You speak like one and you are so confident and cocky. Talk to people in playful way but keep sentence short", 
-        input=msg.message,
+        input=current_session.get_messages(),
         temperature=0.3,
         max_output_tokens=50
     )
-    print(response)
+    current_session.add_message({
+        "role": "assistant",
+        "content": response.output_text
+    })
     reply = response.output_text
     return {"reply": reply}
